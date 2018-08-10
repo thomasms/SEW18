caa3f5b127b787d87107b9c253accd599f9f4219
thomasms <stainer.tom@gmail.com>
2018-08-10 15:05:36 +0100

    Added empty test case

---
+++ exercises/1/numericaltest.py
@@ -0,0 +1,12 @@
+import unittest
+import math
+from numerical import isfloat, getfloat
+
+
+class NumericalUnitTest(unittest.TestCase):
+
+    def test_isfloat(self):
+        self.assertTrue(False, "Test not implemented!")
+
+    def test_getfloat(self):
+        self.assertTrue(False, "Test not implemented!")
